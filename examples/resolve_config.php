0827e1069582cc17fd9afe3317015d72f3f3d03e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-24 22:54:48 +0200

    Resolve config

---
+++ examples/resolve_config.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+$configFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config.php';
+
+if (!file_exists($configFile)) {
+    echo 'No config file found, copy config.sample.php to config.php and your Twitter OAuth details.', PHP_EOL;
+    exit(1);
+}
+
+return require $configFile;
