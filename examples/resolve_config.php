80922ab84a066ea3642d0fb5512787521d47fdc9
Sven Luijten <svenluijten@users.noreply.github.com>
2016-10-26 08:48:48 +0200

    Update resolve_config.php

--- examples/resolve_config.php
+++ examples/resolve_config.php
@@ -3,7 +3,7 @@
 $configFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config.php';
 
 if (!file_exists($configFile)) {
-    echo 'No config file found, copy config.sample.php to config.php and your Twitter OAuth details.', PHP_EOL;
+    echo 'No config file found, copy config.sample.php to config.php and add your Twitter OAuth details.', PHP_EOL;
     exit(1);
 }
 
