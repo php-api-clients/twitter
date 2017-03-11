43a40b4e09a50465b56a89a49482abff504db734
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-11 13:08:02 +0100

    Updated dependencies and now using user agent middleware for the user agent header

--- src/ApiSettings.php
+++ src/ApiSettings.php
@@ -6,9 +6,11 @@ use ApiClients\Foundation\Hydrator\Options as HydratorOptions;
 use ApiClients\Foundation\Options;
 use ApiClients\Foundation\Transport\Middleware\JsonDecodeMiddleware;
 use ApiClients\Foundation\Transport\Options as TransportOptions;
-use ApiClients\Foundation\Transport\UserAgentStrategies;
 use ApiClients\Middleware\Oauth1\Oauth1Middleware;
 use ApiClients\Middleware\Oauth1\Options as Oauth1Options;
+use ApiClients\Middleware\UserAgent\Options as UserAgentMiddlewareOptions;
+use ApiClients\Middleware\UserAgent\UserAgentMiddleware;
+use ApiClients\Middleware\UserAgent\UserAgentStrategies;
 use ApiClients\Tools\Psr7\Oauth1\Definition;
 
 final class ApiSettings
@@ -23,8 +25,17 @@ final class ApiSettings
         Options::TRANSPORT_OPTIONS => [
             TransportOptions::HOST => 'api.twitter.com',
             TransportOptions::PATH => '/1.1/',
-            TransportOptions::USER_AGENT_STRATEGY => UserAgentStrategies::PACKAGE_VERSION,
-            TransportOptions::PACKAGE => 'api-clients/twitter',
+            TransportOptions::MIDDLEWARE => [
+                Oauth1Middleware::class,
+                JsonDecodeMiddleware::class,
+                UserAgentMiddleware::class,
+            ],
+            TransportOptions::DEFAULT_REQUEST_OPTIONS => [
+                UserAgentMiddleware::class => [
+                    UserAgentMiddlewareOptions::STRATEGY => UserAgentStrategies::PACKAGE_VERSION,
+                    UserAgentMiddlewareOptions::PACKAGE => 'api-clients/twitter',
+                ],
+            ],
         ],
     ];
 
@@ -37,8 +48,6 @@ final class ApiSettings
         // @codingStandardsIgnoreStart
         $options = array_replace_recursive(self::TRANSPORT_OPTIONS, $suppliedOptions);
         $options[Options::HYDRATOR_OPTIONS][HydratorOptions::NAMESPACE_SUFFIX] = $suffix;
-        $options[Options::TRANSPORT_OPTIONS][TransportOptions::MIDDLEWARE][] = Oauth1Middleware::class;
-        $options[Options::TRANSPORT_OPTIONS][TransportOptions::MIDDLEWARE][] = JsonDecodeMiddleware::class;
         $options[Options::TRANSPORT_OPTIONS][TransportOptions::MIDDLEWARE] = array_unique(
             $options[Options::TRANSPORT_OPTIONS][TransportOptions::MIDDLEWARE]
         );
